057350b063125bd582b84ea4f353081b42b594df
kelv01 <kelvinnjugunanungari@gmail.com>
2025-11-15 19:42:03 +0300

    added postmodals code

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,9 +1,17 @@
 import PostCard from "@/components/common/PostCard";
+import PostModal from "@/components/common/PostModal";
 import Header from "@/components/layout/Header";
-import { PostProps } from "@/interfaces";
+import { PostProps, PostData } from "@/interfaces";
+import { useState } from "react";
 
 const Posts: React.FC<PostProps[]> = ({ posts }) => {
-  console.log(posts);
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [post, setPost] = useState<PostData | null>(null);
+
+    const handleAddPost = (newPost: PostData) => {
+    setPost({ ...newPost, id: posts.length + 1 });
+  };
+
 
   return (
     <div className="flex flex-col h-screen">
@@ -11,7 +19,10 @@ const Posts: React.FC<PostProps[]> = ({ posts }) => {
       <main className="p-4">
         <div className="flex justify-between">
           <h1 className="text-2xl font-semibold">Post Content</h1>
-          <button className="bg-blue-700 px-4 py-2 rounded-full text-white">
+          <button
+            onClick={() => setModalOpen(true)}
+            className="bg-blue-700 px-4 py-2 rounded-full text-white"
+          >
             Add Post
           </button>
         </div>
@@ -27,6 +38,12 @@ const Posts: React.FC<PostProps[]> = ({ posts }) => {
           ))}
         </div>
       </main>
+      {isModalOpen && (
+        <PostModal
+          onClose={() => setModalOpen(false)}
+          onSubmit={handleAddPost}
+        />
+      )}
     </div>
   );
 };
@@ -42,14 +59,14 @@ const Posts: React.FC<PostProps[]> = ({ posts }) => {
 //   }
 // }
 export async function getStaticProps() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts")
-  const posts = await response.json()
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json();
 
   return {
     props: {
-      posts
-    }
-  }
+      posts,
+    },
+  };
 }
 
 export default Posts;
