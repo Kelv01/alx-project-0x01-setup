71b056fa10721e12ae33727dbc0a04c23800359c
kelv01 <kelvinnjugunanungari@gmail.com>
2025-11-15 21:21:39 +0300

    added userdata

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -72,6 +72,6 @@ export interface UserData {
 }
 export interface UserModalProps {
   onClose: () => void;
-  onSubmit: (post: UserData) => void;
+  onSubmit: (post: UserProps) => void;
   isOpen: boolean;
 }
